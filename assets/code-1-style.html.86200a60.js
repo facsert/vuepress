c136e9dd7c1fbb24b18ace235090744655521216
facser <facsert@outlook.com>
2022-09-18 10:56:35 +0000

    Deploying to gh-branch from @ facser/facser.github.io@9fa0bc51f010b34584fbe41533cc40c056724954 🚀

---
+++ assets/code-1-style.html.86200a60.js
@@ -0,0 +1,67 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as n,c as i,a as s}from"./app.aff7e316.js";const a={},d=s(`<h1 id="\u4EE3\u7801\u98CE\u683C" tabindex="-1"><a class="header-anchor" href="#\u4EE3\u7801\u98CE\u683C" aria-hidden="true">#</a> \u4EE3\u7801\u98CE\u683C</h1><ul><li>\u547D\u540D</li><li>\u6CE8\u91CA</li><li>\u5BA1\u7F8E</li></ul><h2 id="\u547D\u540D" tabindex="-1"><a class="header-anchor" href="#\u547D\u540D" aria-hidden="true">#</a> \u547D\u540D</h2><p>\u76EE\u6807: \u628A\u503C\u7684\u4FE1\u606F\u653E\u5165\u540D\u5B57\u91CC</p><ul><li>\u51C6\u786E</li><li>\u7A81\u51FA\u91CD\u70B9</li><li>\u4E00\u81F4</li><li>\u957F\u5EA6</li></ul><h3 id="\u51C6\u786E" tabindex="-1"><a class="header-anchor" href="#\u51C6\u786E" aria-hidden="true">#</a> \u51C6\u786E</h3><p>\u4F7F\u7528 <strong>\u51C6\u786E</strong> <strong>\u5177\u4F53</strong> <strong>\u5355\u4E00</strong>\u7684\u8BCD</p><p>\u53D8\u91CF\u547D\u540D \u540D\u8BCD \u5F62\u5BB9\u8BCD \u51FD\u6570 \u52A8\u8BCD \u540D\u8BCD</p><p><a href="#%E5%B8%B8%E7%94%A8%E5%8A%A8%E8%AF%8D">\u5E38\u7528\u52A8\u8BCD</a></p><h3 id="\u7A81\u51FA\u91CD\u70B9" tabindex="-1"><a class="header-anchor" href="#\u7A81\u51FA\u91CD\u70B9" aria-hidden="true">#</a> \u7A81\u51FA\u91CD\u70B9</h3><p>\u901A\u8FC7 \u524D\u7F00 \u540E\u7F00 \u7A81\u51FA\u91CD\u70B9</p><table><thead><tr><th style="text-align:center;">\u524D\u7F00</th><th style="text-align:center;">\u542B\u4E49</th></tr></thead><tbody><tr><td style="text-align:center;">is_xx</td><td style="text-align:center;">\u662F\u4E0D\u662F</td></tr><tr><td style="text-align:center;">has_xx</td><td style="text-align:center;">\u6709\u6CA1\u6709</td></tr><tr><td style="text-align:center;">should_xx</td><td style="text-align:center;">\u662F\u5426\u5E94\u8BE5</td></tr><tr><td style="text-align:center;">can_xx</td><td style="text-align:center;">\u80FD\u4E0D\u80FD</td></tr></tbody></table><p>\u907F\u514D\u53CD\u4E49\u547D\u540D</p><div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code> test_pass <span class="token operator">=</span> <span class="token boolean">True</span>                                <span class="token comment"># \u63A8\u8350 True \u4E00\u822C\u4E0E\u6210\u529F, \u6B63\u5E38\u5BF9\u5E94 </span>
+ test_fail <span class="token operator">=</span> <span class="token boolean">False</span>                               <span class="token comment"># \u4E0D\u63A8\u8350 False \u4E00\u822C\u4E0E\u9519\u8BEF, \u5931\u8D25\u5BF9\u5E94</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>\u5EA6\u91CF\u547D\u540D\u5E26\u4E0A\u5355\u4F4D</p><div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code> time_secs <span class="token operator">=</span> <span class="token number">8</span>
+ time_hours <span class="token operator">=</span> <span class="token number">2</span>
+ time_days <span class="token operator">=</span> <span class="token number">3</span> 
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u4E00\u81F4" tabindex="-1"><a class="header-anchor" href="#\u4E00\u81F4" aria-hidden="true">#</a> \u4E00\u81F4</h3><h3 id="\u957F\u5EA6" tabindex="-1"><a class="header-anchor" href="#\u957F\u5EA6" aria-hidden="true">#</a> \u957F\u5EA6</h3><p>\u5728\u5C0F\u7684\u4F5C\u7528\u533A\u57DF\u5185\u53EF\u4EE5\u4F7F\u7528\u7B80\u77ED\u7684\u53D8\u91CF\u540D</p><p>\u4F7F\u7528\u5E38\u7528\u7684\u5355\u8BCD\u7F29\u5199</p><p><a href="#%E5%8D%95%E8%AF%8D%E7%BC%A9%E5%86%99">\u5E38\u89C1\u5355\u8BCD\u7F29\u5199</a></p><h2 id="\u6CE8\u91CA" tabindex="-1"><a class="header-anchor" href="#\u6CE8\u91CA" aria-hidden="true">#</a> \u6CE8\u91CA</h2><p>\u6CE8\u91CA</p><h2 id="\u5BA1\u7F8E" tabindex="-1"><a class="header-anchor" href="#\u5BA1\u7F8E" aria-hidden="true">#</a> \u5BA1\u7F8E</h2><ul><li>\u4F7F\u7528\u4E00\u81F4\u7684\u5E03\u5C40</li><li>\u4EE3\u7801\u5206\u7EC4, \u5F62\u6210\u4EE3\u7801\u5757</li></ul><h1 id="\u9644\u5F55" tabindex="-1"><a class="header-anchor" href="#\u9644\u5F55" aria-hidden="true">#</a> \u9644\u5F55</h1><h2 id="\u5E38\u7528\u52A8\u8BCD" tabindex="-1"><a class="header-anchor" href="#\u5E38\u7528\u52A8\u8BCD" aria-hidden="true">#</a> \u5E38\u7528\u52A8\u8BCD</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>    \u6DFB\u52A0/\u63D2\u5165                      add, append, insert, 
+    \u521B\u5EFA/\u521D\u59CB\u5316                    create, initialize, 
+    \u52A0\u8F7D/\u9009\u62E9                      load, pick, <span class="token keyword">select</span>
+    \u5220\u9664/\u9500\u6BC1                      delete, remove, destroy, drop
+    \u6253\u5F00/\u542F\u52A8                      open, start
+    \u5173\u95ED/\u505C\u6B62                      close, stop
+    \u83B7\u53D6/\u8BFB\u53D6                      get, fetch, acquire, read, 
+    \u67E5\u627E/\u67E5\u8BE2                      search, find, query
+    \u8BBE\u7F6E/\u91CD\u7F6E                      set, reset
+    \u653E\u5165/\u5199\u5165                      put, write, 
+    \u91CA\u653E/\u5237\u65B0                      release, refresh
+    \u53D1\u9001/\u63A8\u9001                      send, push
+    \u63A5\u6536/\u62C9\u53D6                      receive, pull
+    \u63D0\u4EA4/\u53D6\u6D88                      submit, cancel
+    \u6536\u96C6/\u91C7\u96C6                      collect, 
+    \u63D0\u53D6/\u89E3\u6790                      sub, extract, parse
+    \u7F16\u7801/\u89E3\u7801                      encode, decode
+    \u586B\u5145/\u538B\u7F29                      fill, pack, compress
+    \u6E05\u7A7A/\u89E3\u538B                      flush, clear, unpack, decompress
+    \u589E\u52A0/\u51CF\u5C11                      increase, decrease, reduce
+    \u5206\u9694/\u62FC\u63A5                      split, join, concat
+    \u8FC7\u6EE4/\u6821\u9A8C/\u68C0\u6D4B                 filter, valid, check
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5355\u8BCD\u7F29\u5199" tabindex="-1"><a class="header-anchor" href="#\u5355\u8BCD\u7F29\u5199" aria-hidden="true">#</a> \u5355\u8BCD\u7F29\u5199</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>    addition              <span class="token function">add</span>               \u52A0
+    subtraction           sub               \u51CF
+    multiplication        mul               \u4E58\u6CD5
+    division              div               \u9664\u6CD5
+    hexadecimal           hex               \u5341\u516D\u8FDB\u5236
+    array                 arr               \u6570\u7EC4, \u96C6\u5408
+    list/Sequence         lst/seq           \u5217\u8868 
+    dictionary            dict              \u5B57\u5178
+    character             char              \u5B57\u7B26
+    string                str               \u5B57\u7B26\u4E32
+    text                  txt               \u6587\u672C
+    number                num               \u6570\u91CF, \u7F16\u53F7
+    image                 img               \u56FE\u50CF
+    length                len               \u957F\u5EA6
+    summation             <span class="token function">sum</span>               \u548C
+    average               avg               \u5E73\u5747
+    maximum               max               \u6700\u5927\u503C
+    minimum               min               \u6700\u5C0F\u503C
+    middle                mid               \u4E2D\u503C
+    <span class="token builtin class-name">source</span>                src               \u6E90\u5934
+    address               addr              \u5730\u5740
+    previous              pre               \u524D\u4E00\u4E2A
+    current               cur               \u5F53\u524D\u7684
+    initalize             init              \u521D\u59CB\u5316
+    database              db                \u6570\u636E\u5E93
+    administrator         adm               \u7BA1\u7406\u5458
+    password              <span class="token builtin class-name">pwd</span>               \u5BC6\u7801
+    user                  usr               \u7528\u6237
+    directory             <span class="token function">dir</span>               \u76EE\u5F55
+    document              doc               \u6587\u6863
+    library               lib               \u5E93
+    <span class="token keyword">function</span>              func              \u51FD\u6570
+    object                obj               \u5BF9\u8C61
+    argument              arg               \u5B9E\u53C2
+    variable              var               \u53D8\u91CF
+    parameter             param             \u53C2\u6570<span class="token punctuation">(</span>\u5F62\u53C2<span class="token punctuation">)</span>
+    execute               <span class="token builtin class-name">exec</span>              \u6267\u884C
+    <span class="token builtin class-name">command</span>               cmd               \u547D\u4EE4
+    configuration         config            \u914D\u7F6E
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,30),l=[d];function r(t,c){return n(),i("div",null,l)}const o=e(a,[["render",r],["__file","code-1-style.html.vue"]]);export{o as default};
